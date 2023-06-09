87d9b1c6664d31263a0f69be07c468aa7a5c7a1b
Nathan Boswell <nathan@Nathans-MacBook-Air.local>
2023-06-09 15:51:47 +0100

    Fixes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -34,7 +34,7 @@ lsp.on_attach(function(_, bufnr) -- _ is client
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 
     vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
