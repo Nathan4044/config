8e3d9072bd96759a8a30d1c3227fb460852c1c2c
Nathan Boswell <nathan@Nathans-MacBook-Air.local>
2023-09-23 09:38:34 +0100

    Add live grep

--- nvim/lua/nathan/remap.lua
+++ nvim/lua/nathan/remap.lua
@@ -7,3 +7,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>tn", vim.cmd.tabnew)
