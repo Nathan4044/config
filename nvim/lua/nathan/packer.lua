578bc4e79b924cdb5f28ac54377cc33ae7a0e7f7
Nathan4044 <nathan4044@gmail.com>
2023-10-27 10:33:39 +0100

    Update telescope version

--- nvim/lua/nathan/packer.lua
+++ nvim/lua/nathan/packer.lua
@@ -8,8 +8,7 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
+		'nvim-telescope/telescope.nvim', branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
@@ -41,11 +40,16 @@ return require('packer').startup(function(use)
 				end,
 			},
 			{'williamboman/mason-lspconfig.nvim'}, -- Optional
-	
+
 			-- Autocompletion
 			{'hrsh7th/nvim-cmp'},     -- Required
 			{'hrsh7th/cmp-nvim-lsp'}, -- Required
 			{'L3MON4D3/LuaSnip'},     -- Required
 		}
 	}
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
