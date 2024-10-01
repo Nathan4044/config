009f1c52a65f229b02cab8a4383f4d157de247ff
Nathan Boswell <nathan4044@gmail.com>
2024-10-01 21:45:19 +0100

    Add initial fish configuration

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -gx emacs
+    set -gx EDITOR nvim
+    set -gx VISUAL $EDITOR
+
+    function ls
+        command ls -aG $argv
+    end
+
+    function l
+        command ls $argv
+    end
+
+    function vim
+        command nvim $argv
+    end
+
+    function view
+        command nvim -R $argv
+    end
+
+    function python
+        command python3 $argv
+    end
+
+    function update
+        brew update
+        brew upgrade
+        brew autoremove
+        brew cleanup
+    end
+end
