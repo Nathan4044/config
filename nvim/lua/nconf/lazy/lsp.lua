8f1d80cccd479bea65723ae61f4163b4169ffa4d
Nathan Boswell <nathan4044@gmail.com>
2025-08-05 19:30:55 +0100

    Add default clangd format style

--- nvim/lua/nconf/lazy/lsp.lua
+++ nvim/lua/nconf/lazy/lsp.lua
@@ -1,100 +1,107 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  dependencies = {
-    -- LSP Support
-    'neovim/nvim-lspconfig',             -- Required
-    -- 'williamboman/mason.nvim',           -- Optional
-    'williamboman/mason-lspconfig.nvim', -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    dependencies = {
+        -- LSP Support
+        'neovim/nvim-lspconfig',             -- Required
+        -- 'williamboman/mason.nvim',           -- Optional
+        'williamboman/mason-lspconfig.nvim', -- Optional
 
-    -- -- Autocompletion
-    'hrsh7th/nvim-cmp',     -- Required
-    'hrsh7th/cmp-nvim-lsp', -- Required
-  },
+        -- -- Autocompletion
+        'hrsh7th/nvim-cmp',     -- Required
+        'hrsh7th/cmp-nvim-lsp', -- Required
+    },
 
-  config = function()
-    -- Add cmp_nvim_lsp capabilities settings to lspconfig
-    -- This should be executed before you configure any language server
-    local lspconfig = require('lspconfig')
-    local lspconfig_defaults = lspconfig.util.default_config
+    config = function()
+        -- Add cmp_nvim_lsp capabilities settings to lspconfig
+        -- This should be executed before you configure any language server
+        local lspconfig = require('lspconfig')
+        local lspconfig_defaults = lspconfig.util.default_config
 
-    lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-      'force',
-      lspconfig_defaults.capabilities,
-      require('cmp_nvim_lsp').default_capabilities()
-    )
+        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+            'force',
+            lspconfig_defaults.capabilities,
+            require('cmp_nvim_lsp').default_capabilities()
+        )
 
-    lspconfig.lua_ls.setup {
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {
-              'vim',
-              'require'
+        lspconfig.lua_ls.setup {
+            settings = {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = {
+                            'vim',
+                            'require'
+                        },
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
+                },
             },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    }
+        }
 
-    require('mason').setup({})
-    require('mason-lspconfig').setup({
-      handlers = {
-        function(server_name)
-          lspconfig[server_name].setup({})
-        end,
-      },
-    })
+        lspconfig.clangd.setup {
+            cmd = {
+                "clangd",
+                "--fallback-style=webkit"
+            }
+        }
 
-    -- This is where you enable features that only work
-    -- if there is a language server active in the file
-    vim.api.nvim_create_autocmd('LspAttach', {
-      desc = 'LSP actions',
-      callback = function(event)
-        local opts = {buffer = event.buf}
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            handlers = {
+                function(server_name)
+                    lspconfig[server_name].setup({})
+                end,
+            },
+        })
 
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-        vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-        vim.keymap.set("n", "fm", function() vim.lsp.buf.format({async = true}) end, opts)
+        -- This is where you enable features that only work
+        -- if there is a language server active in the file
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = { buffer = event.buf }
 
-        vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count=1, float=true}) end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count=-1, float=true}) end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+                vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+                vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+                vim.keymap.set("n", "fm", function() vim.lsp.buf.format({ async = true }) end, opts)
 
-        vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-      end,
-    })
+                vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
 
-    -- Autocompletion
-    local cmp = require('cmp')
-    cmp.setup({
-      sources = {
-        {name = 'nvim_lsp'},
-      },
-      snippet = {
-        expand = function(args)
-          -- You need Neovim v0.10 to use vim.snippet
-          vim.snippet.expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({}),
-    })
-  end
+                vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+            end,
+        })
+
+        -- Autocompletion
+        local cmp = require('cmp')
+        cmp.setup({
+            sources = {
+                { name = 'nvim_lsp' },
+            },
+            snippet = {
+                expand = function(args)
+                    -- You need Neovim v0.10 to use vim.snippet
+                    vim.snippet.expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({}),
+        })
+    end
 }
