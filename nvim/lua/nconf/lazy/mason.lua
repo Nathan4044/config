3a8da65107475a6b95e699a78288798499bd99bf
Nathan Boswell <nathan4044@gmail.com>
2025-11-01 09:55:23 +0000

    format

--- nvim/lua/nconf/lazy/mason.lua
+++ nvim/lua/nconf/lazy/mason.lua
@@ -1,6 +1,6 @@
 return {
-  'williamboman/mason.nvim',
-  config = function()
-    pcall(vim.cmd, 'MasonUpdate')
-  end
+    'williamboman/mason.nvim',
+    config = function()
+        pcall(vim.cmd, 'MasonUpdate')
+    end
 }
