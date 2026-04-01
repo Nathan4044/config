1e77f38f05f0d6062c5f2000676da4842c3e8913
Nathan Boswell <nboswell@roku.com>
2026-04-01 16:07:33 +0100

    Update treesitter config for nvim 0.12

--- nvim/lua/nconf/init.lua
+++ nvim/lua/nconf/init.lua
@@ -1,5 +1,22 @@
 vim.g.mapleader = " "
 
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    -- 1. Get the filetype of the current buffer
+    local ft = args.match
+
+    -- 2. Check if a Treesitter parser exists for this filetype
+    -- vim.treesitter.language.get_lang returns the parser name if found
+    local parser_exists = pcall(vim.treesitter.language.get_lang, ft)
+
+    if parser_exists then
+      -- 3. If a parser exists, attempt to start treesitter highlighting
+      -- We use pcall to ensure this doesn't error out on unsupported filetypes
+      pcall(vim.treesitter.start)
+    end
+  end,
+})
+
 require("nconf.lazy_init")
 require("nconf.set")
 require("nconf.remap")
--- nvim/lua/nconf/lazy/treesitter.lua
+++ nvim/lua/nconf/lazy/treesitter.lua
@@ -3,35 +3,8 @@ return {
     build = ":TSUpdate",
 
     config = function()
-        require("nvim-treesitter").setup({
-            -- A list of parser names, or "all" (the three listed parsers should always be installed)
-            ensure_installed = { "c", "lua", "go" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-
-            indent = {
-                enable = true,
-                -- disable = { "python", "c" } -- these and some other langs don't work well
-            },
-
-            ignore_install = {},
-
-            modules = {},
+        require("nvim-treesitter.config").setup({
+            auto_install = true, -- This is the magic switch
         })
     end
 }
